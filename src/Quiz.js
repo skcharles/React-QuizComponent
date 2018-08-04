810219df84c4bd43bbdca4fde65ee58b35dfbe3d
Senthilkumar Charles <senthilkumarcharles@Senthilkumars-MacBook-Pro.local>
2018-08-04 11:45:55 -0500

    QuizEnd Component Implemented

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,7 +1,8 @@
 import React, {Component} from 'react';
 import QuizQuestion from './QuizQuestion.js'
+import QuizEnd from './QuizEnd.js';
 let quizData = require('./quiz_data.json');
-
+const isQuizEnd = false;
 class Quiz extends Component {
 
     constructor(props) {
@@ -12,8 +13,9 @@ class Quiz extends Component {
         }
     }
     render() {
+        const content = isQuizEnd ? <QuizEnd/> : <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position-1]}/>
         return (<div>
-            <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position-1]}/>
+            {content}
         </div>);
     }
 }
