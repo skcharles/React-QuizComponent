09a0a2a9e759ceca469ff37c29376facc9eea098
Senthilkumar Charles <senthilkumarcharles@Senthilkumars-MacBook-Pro.local>
2018-08-05 00:51:41 -0500

    Reset

--- src/Quiz.js
+++ src/Quiz.js
@@ -12,6 +12,9 @@ class Quiz extends Component {
         }
     }
 
+    handleResetClick() {
+        this.setState({quiz_position:1});
+    }
     showNextQuestion() {
         this.setState((state) => { return {quiz_position: state.quiz_position + 1}});
     }
@@ -19,7 +22,7 @@ class Quiz extends Component {
         const isQuizEnd = (this.state.quiz_position - 1 === quizData.quiz_questions.length);
         return (<div>
             {isQuizEnd ?
-             <QuizEnd/> : 
+             <QuizEnd resetClickHandler={this.handleResetClick.bind(this)}/> : 
              <QuizQuestion 
                 quiz_question={quizData.quiz_questions[this.state.quiz_position-1]}
                 showNextQuestionHandler={this.showNextQuestion.bind(this)}
